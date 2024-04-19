a7c5e754cc682c2c9998a2a4bfa0cc69969e4a2a
ritzbhuj@gmail.com <ritzbhuj@gmail.com>
2024-04-19 22:27:15 +0200

    First commit

---
+++ loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main()
+{
+	int outside = 0;
+	printf ("Initial value of variable outside %d:\n",outside);
+	for ( int i = 0; i < 10; i++ )
+	{
+		printf ("Current value of pointer i %d:\n",i);
+	}
+}
